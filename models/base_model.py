4e3211a87e36ca959852bacc25811988422a8c73
nassfatie <nassfatie@gmail.com>
2023-02-09 12:33:51 -0800

    class attributes updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Represents the BaseModel of the project."""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
